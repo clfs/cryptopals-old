3adc51ebc04544ccc3f47f3303607d459b6606ba
Calvin Figuereo-Supraner <mail@calvin.page>
2019-01-26 16:40:33 -0800

    change docstring to comment, 13

--- test_13.py
+++ test_13.py
@@ -25,13 +25,12 @@ def kvparse(s):
 
 
 def create_admin_profile(manager):
-    """Assume attacker knows AES encryption is used."""
+    # Assume attacker knows AES encryption is used.
     magic_block = pad(b"admin", 16).decode()
     email1 = "A" * (16 - len("email=")) + magic_block
     email2 = "A" * (32 - len("email=" + "&uid=10&role="))
     profile1 = manager.create_profile(email1)
     profile2 = manager.create_profile(email2)
-
     # This is the cut-and-paste step.
     return profile2[:-16] + profile1[16:32]
 
